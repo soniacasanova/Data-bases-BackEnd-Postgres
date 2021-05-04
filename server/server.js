fcf3ae242053b82fdbc2ec5b5ec013c090d14aee
sonia casanova <soniacasanova@mbp-de-sonia-2.home>
2021-05-04 10:17:40 +0200

    data bases-third commit

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-const db = require('./db');
+const db = require("./db");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-
-
+app.use(express.json()); //convierte el body(json) en un objeto js
 
 app.get("/forums", async (req, res) => {
-  const forums = await db.getForums();
-  res.json(forums);
+  try {
+    const forums = await db.getForums();
+    res.json({ status: "OK", results: forums });
+  } catch (e) {
+    res.status(500).json({ status: "ERROR", details: e.toString() });
+  }
+});
+
+app.post("/forums", async (req, res) => {
+  // validamos que lo que envian está bien
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ status: "ERROR", details: "Missing 'name' field" });
+  }
 });
 
 app.listen(8080, () => {
